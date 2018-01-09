8fc0a60c3705fcc834cb669471b8924a65211adf
tom-aglow <tom@ohhhh.me>
2018-01-08 22:31:49 -0800

    add navigation component and styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
 import works from '../db/works';
+import NavigationLink from './NavigationLink';
 
 class Navigation extends Component {
   render() {
-    const links = works.map(({link}) => <Link to={link} className="thumb" />);
+    const links = works.map((work, index) => (
+      <NavigationLink key={index} {...work} />
+    ));
     return <nav className="thumbs">{links}</nav>;
   }
 }
---
+++ src/components/NavigationLink.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class NavigationLink extends Component {
+  state = {
+    style: {}
+  };
+
+  addThumbBackground = async () => {
+    const thumb = await import(`../img/thumbnails/${this.props.thumb}`);
+    this.setState({style: {backgroundImage: `url(${thumb})`}});
+  };
+
+  removeThumbBackground = () => {
+    this.setState({style: {}});
+  };
+
+  render() {
+    return (
+      <Link
+        to={this.props.link}
+        className="thumb"
+        style={this.state.style}
+        onMouseEnter={this.addThumbBackground}
+        onMouseLeave={this.removeThumbBackground}
+      />
+    )
+  }
+}
+
+export default NavigationLink;
