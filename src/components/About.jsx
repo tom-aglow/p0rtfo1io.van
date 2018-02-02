9f55a12f2707ace7f72166b4aa335524185e2278
tom-aglow <tom@ohhhh.me>
2018-02-01 20:39:53 -0800

    add quote on about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 const About = () => {
   return (
@@ -8,9 +9,9 @@ const About = () => {
         <p>yes, you’ve read right. i have the engineering degree with honours in the space industry. and i was supposed to build rockets and launch them into space, but instead i’m building websites and launch them to the world wide web.</p>
         <p>why? i guess it would be your next question. well, i can tell there are some similarities between space and the web. they both are vast and full of danger, so your product should be well-designed in order to succeed its goals either it is mars mission or start-up idea.</p>
         <p>creating a rocket or a website is an endeavour that is impossible to accomplish without careful planning and meeting deadlines. it demands you to possess certain skillset and abilities. attention to details is one of them, because both a rocket and a website contains numerous little elements that you must take care of.</p>
-        <p>as an engineer, i dreamt to be involved in some great space program such as a new space shuttle or colonization of the moon. as a web developer, i would like to become a part of a team where i can grow as a programmer and make a significant contribution to company’s business.</p>
-        <p>if you are looking for a developer with an engineering background or need someone to strengthen your team, please feel free to reach me out at <a
-          href="mailto: tom@ohhhh.me">tom@ohhhh.me</a></p>
+        <p>but there is one slight difference between rocket engineering and web development. the last one has more space to apply your creativity. and i believe i have a lot that kind of thing, so i switched to creating creative websites a while ago. and now i’m seeking an environment which will allow me to contribute my unique combination of my creativity and technical skills in everything i participate in.</p>
+        <p>to interview me, email at <a href="mailto: tom@ohhhh.me">tom@ohhhh.me</a></p>
+        <p className="quote"><em>“i code therefore i am”</em> - tom, {moment().format('MMMM Do YYYY')}</p>
       </div>
     </div>
   );
